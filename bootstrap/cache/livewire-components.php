dda6bcc5ece055861626244e1f339cc9273ed68a
Aymanebenyahia <133663543+Aymanebenyahia@users.noreply.github.com>
2023-06-25 00:45:41 +0200

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'add-product-component' => 'App\\Http\\Livewire\\AddProductComponent',
+  'add-purchase-component' => 'App\\Http\\Livewire\\AddPurchaseComponent',
+  'add-quotation-component' => 'App\\Http\\Livewire\\AddQuotationComponent',
+  'add-sub-category-component' => 'App\\Http\\Livewire\\AddSubCategoryComponent',
+  'category-list-component' => 'App\\Http\\Livewire\\CategoryListComponent',
+  'fournissuer-list-component' => 'App\\Http\\Livewire\\FournissuerListComponent',
+  'new-fournissuer-component' => 'App\\Http\\Livewire\\NewFournissuerComponent',
+  'perchase-list-component' => 'App\\Http\\Livewire\\PerchaseListComponent',
+  'product-component' => 'App\\Http\\Livewire\\ProductComponent',
+  'quotation-list-component' => 'App\\Http\\Livewire\\QuotationListComponent',
+  'sub-category-list-component' => 'App\\Http\\Livewire\\SubCategoryListComponent',
+);
